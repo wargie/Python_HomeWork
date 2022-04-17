e5b3f96c7ace58e99ae2c8be667b24ca792fab30
wargie <wargie@bk.ru>
2022-04-17 16:29:34 +0200

    Добавлены задачи, решённые входе первого семинарского занятия

---
+++ task_000.py
@@ -0,0 +1,4 @@
+from random import randint
+a = randint(0, 100)
+print(f'Сгенерировано случайное число равное {a}')
+print(f'Квадрат числа {a} равен {a**2}')
---
+++ task_003.py
@@ -0,0 +1,4 @@
+from random import randint
+a = randint(0, 100)
+for i in range(-a, a+1):
+    print(i)

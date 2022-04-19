4cc60bd6427ef76d831a55c1af3c2a35fdfb3792
wargie <wargie@bk.ru>
2022-04-19 19:20:53 +0200

    Оптимизирован и офрмлен код к задаче №9

--- task_009.py
+++ task_009.py
@@ -1,8 +1,7 @@
 #Указав номер четверти прямоугольной системы координат, показать допустимые значения координат для точек этой четверти
 import random
 quarter = random.randint(1, 4)
-print(f'Сгенерирован случайный номер четверти = {quarter}')
-#quarter = int(input())
+print(f'Сгенерирован случайный номер четверти равный {quarter}')
 if quarter == 1:
     zakres = "x > 0, y > 0"
 elif quarter == 2:
@@ -11,4 +10,4 @@ elif quarter == 3:
     zakres = "x < 0, y < 0"
 elif quarter == 4:
     zakres = "x > 0, y < 0"
-print(f'Для {quarter}-й четверти координаты точки должны быть заданы в диапазоне ({zakres})')
+print(f'Для {quarter}-й четверти координаты точки лежат в диапазоне в диапазоне ({zakres})')

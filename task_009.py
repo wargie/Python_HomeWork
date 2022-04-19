fa310cd197fbe3f7409ee7027fe5a230e9f0af1c
wargie <wargie@bk.ru>
2022-04-19 19:17:45 +0200

    Добавлено решение задачи №9

--- task_009.py
+++ task_009.py
@@ -1,1 +1,14 @@
 #Указав номер четверти прямоугольной системы координат, показать допустимые значения координат для точек этой четверти
+import random
+quarter = random.randint(1, 4)
+print(f'Сгенерирован случайный номер четверти = {quarter}')
+#quarter = int(input())
+if quarter == 1:
+    zakres = "x > 0, y > 0"
+elif quarter == 2:
+    zakres = "x < 0, y > 0"
+elif quarter == 3:
+    zakres = "x < 0, y < 0"
+elif quarter == 4:
+    zakres = "x > 0, y < 0"
+print(f'Для {quarter}-й четверти координаты точки должны быть заданы в диапазоне ({zakres})')
